6b9d437f8b29f45406c9ebe84cecae256eb92994
hhxia <annie_start@163.com>
2020-01-14 17:59:13 +0800

    首页完成

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,26 @@
 </template>
 
 <script>
-
+// import storage from './Storage/index'
 export default {
   data() {
     return {
-     
+     data:''
     }
+  },
+  mounted() {
+    // this.data = Storage.getItem('mall')
+    // storage.setItem('cars',{name:'zs',type:1},'user')
+    // storage.clear('a','user')
+    
   }
 
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/scss/reset.scss';
+@import './assets/scss/config.scss';
+@import './assets/scss/button.scss';
 
 </style>
